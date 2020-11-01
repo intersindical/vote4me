c6bd3cc71279f77dfb25f4a0288716181367c842
karasu <gustaucastells@gmail.com>
2020-11-01 10:31:38 +0100

    Working on codes encryption/decryption in php

--- decrypt_codes.php
+++ decrypt_codes.php
@@ -1,11 +1,60 @@
-function decrypt($key, $iv, $ciphertext) {
-    $method = "AES-256-CBC";
+<?php 
 
-    return openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv);
+function encrypt($plaintext, $key) {
+    $ivlen = openssl_cipher_iv_length("AES-256-CBC");
+    $iv = openssl_random_pseudo_bytes($ivlen);
+
+    $ciphertext_raw = openssl_encrypt(
+        $plaintext, "AES-256-CBC", $key, $options=OPENSSL_RAW_DATA, $iv);
+    
+    $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+
+    return base64_encode($iv.$hmac.$ciphertext_raw);
 }
 
+function decrypt($ciphertext, $key) {
+    $c = base64_decode($ciphertext);
+    $ivlen = openssl_cipher_iv_length("AES-256-CBC");
+    $iv = substr($c, 0, $ivlen);
+    $hmac = substr($c, $ivlen, $sha2len=32);
+    $ciphertext_raw = substr($c, $ivlen+$sha2len);
+    
+    $original_plaintext = openssl_decrypt(
+        $ciphertext_raw, "AES-256-CBC", $key, $options=OPENSSL_RAW_DATA, $iv);
 
-$key = hex2bin("0110bbc60a4d374b1f09ea33404e52bf");
-$iv = hex2bin("789be74659c51791d9d1bf6163c5edbc");
+    $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+    if (hash_equals($hmac, $calcmac)) {
+        return $original_plaintext;
+    } else {
+        return false;
+    }    
+}
 
-$method = "AES-256-
+function gen_codes() {
+$num_codis = 50;
+$mida_codi_bytes = 16;
+
+for ($i=0; $i<$num_codis; $i++) {
+    $bytes = openssl_random_pseudo_bytes($mida_codi_bytes);
+    $hex   = bin2hex($bytes);
+    //var_dump($hex);
+    echo $hex.", ";
+}
+
+}
+
+$key = openssl_random_pseudo_bytes(16);
+$plaintext = "This is a test!";
+$ciphertext = encrypt($plaintext, $key);
+
+echo "Key: ".bin2hex($key)."\n";
+echo "Text: ".$plaintext."\n";
+echo "Ciphertext: ".$ciphertext."\n";
+
+// decrypt later....
+
+$dectext = decrypt($ciphertext, $key);
+
+echo "Decrypted text: ".$dectext."\n";
+
+?>
