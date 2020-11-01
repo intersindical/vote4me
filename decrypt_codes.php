6b940182c4a1521994b12274607cb5372e38a2b2
karasu <gustaucastells@gmail.com>
2020-11-01 21:22:23 +0100

    Code generation

--- decrypt_codes.php
+++ decrypt_codes.php
@@ -7,52 +7,54 @@ function encrypt($plaintext, $key) {
     $ciphertext_raw = openssl_encrypt(
         $plaintext, "AES-256-CBC", $key, $options=OPENSSL_RAW_DATA, $iv);
     
-    $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+    $hash = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
 
-    return base64_encode($iv.$hmac.$ciphertext_raw);
+    return base64_encode($iv.$hash.$ciphertext_raw);
 }
 
 function decrypt($ciphertext, $key) {
     $c = base64_decode($ciphertext);
     $ivlen = openssl_cipher_iv_length("AES-256-CBC");
     $iv = substr($c, 0, $ivlen);
-    $hmac = substr($c, $ivlen, $sha2len=32);
-    $ciphertext_raw = substr($c, $ivlen+$sha2len);
+    $hash = substr($c, $ivlen, $sha2len=32);
+    $ciphertext_raw = substr($c, $ivlen + $sha2len);
     
     $original_plaintext = openssl_decrypt(
         $ciphertext_raw, "AES-256-CBC", $key, $options=OPENSSL_RAW_DATA, $iv);
 
-    $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-    if (hash_equals($hmac, $calcmac)) {
+    $calc_hash = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+    if (hash_equals($hash, $calc_hash)) {
         return $original_plaintext;
     } else {
         return false;
     }    
 }
 
-function gen_codes($num_codis) {
-
+function gen_codes($num_codis, $key) {
     $mida_codi_bytes = 16;
-
+    $codes = array();
     for ($i=0; $i<$num_codis; $i++) {
         $bytes = openssl_random_pseudo_bytes($mida_codi_bytes);
         $hex   = bin2hex($bytes);
-        echo $hex.", ";
+        $hex_enc = encrypt($hex, $key);
+        $codes[] = $hex_enc;
     }
+    return $codes;
 }
 
 $key = openssl_random_pseudo_bytes(16);
-$plaintext = "This is a test!";
-$ciphertext = encrypt($plaintext, $key);
+$num_codes = 10;
+$codes = gen_codes($num_codes, $key);
 
 echo "Key: ".bin2hex($key)."\n";
-echo "Text: ".$plaintext."\n";
-echo "Ciphertext: ".$ciphertext."\n";
+
+foreach ($codes as $code) {
+    echo $code."\n";
+}
 
 // decrypt later....
 
-$dectext = decrypt($ciphertext, $key);
-
-echo "Decrypted text: ".$dectext."\n";
+//$dectext = decrypt($ciphertext, $key);
+//echo "Decrypted text: ".$dectext."\n";
 
 ?>
